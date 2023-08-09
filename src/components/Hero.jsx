299fb4c372f502c56c3c40ffe7671bca3b72102d
Fabian vargas <fabivargasfuentes@gmail.com>
2023-08-08 20:31:46 -0600

    Mobile production fixes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ import { ComputersCanvas } from "./canvas";
 const Hero = () => {
   const isMobile = window.matchMedia("(max-width: 768px)").matches;
   return (
-    <section className={`relative w-full h-screen mx-auto` /*Fits the background*/}>
+    <section className={`relative w-full ${isMobile ? "h-[40vh]" : "h-screen"} mx-auto` /*Fits the background*/}>
        <div className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`} /*Space where the her text is placed*/>
         <div className='flex flex-col justify-center items-center mt-5' /*Space for the circle with a line next to the text*/>
           <div className='w-5 h-5 rounded-full bg-[#915EFF]' /*The circle*/ />
